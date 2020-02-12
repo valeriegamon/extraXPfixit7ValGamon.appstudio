70b894f96208dcd6e374506ab4bc3789f54763ce
valeriegamon <valeriegamon@valeries-mbp.ddns.creighton.edu>
2020-02-12 16:12:55 -0600

    two

--- forms/bestPet/bestPet.js
+++ forms/bestPet/bestPet.js
@@ -2,14 +2,20 @@
 let bestPetDog = ('Dogs are likely the most stereotypical pet for children – and there"s a reason for that. Most average-sized dogs are large enough to avoid being injured by your child, but many breeds are also calm and laidback with children. Dogs easily live to a decade, so you shouldn"t have to worry about having that uncomfortable conversation with your child. This species is also trainable and can usually go anywhere your child can, so they make good everyday companions.');
 alert(bestPetDog);
 
+//all lowercase = single
 let replacedPetWithHamster = bestPetDog.replace("dog", "hamster");
-alert(replacedPetWithHamster);
+//alert(replacedPetWithHamster);
 
-let replacedPetWithHamster2 = replacedPetWithHamster.replace("Dog", "hamster");
-alert(replacedPetWithHamster2);
+//Uppercase = single
+let replacedPetWithHamster2 = replacedPetWithHamster.replace("Dog", "Hamster");
+//alert(replacedPetWithHamster2);
 
+//all lowercase = many
+let replacedPetWithHamster3 = replacedPetWithHamster.replace("dogs", "hamsters");
+//alert(replacedPetWithHamster3);
 
-
-
+//Uppercase = many
+let replacedPetWithHamster4 = replacedPetWithHamster.replace("Dogs", "Hamsters");
+alert(replacedPetWithHamster4);
 
 
